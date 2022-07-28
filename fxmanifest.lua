4f10c9561821c9d0627f8017d57b4db9dc35360b
Azeroth <76072277+AzerothSixhten@users.noreply.github.com>
2022-07-28 09:53:11 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,44 @@
+fx_version "adamant"
+game "gta5"
+
+shared_scripts {
+    "config.lua"
+}
+
+client_scripts {
+    'lib/RMenu.lua',
+    'lib/menu/RageUI.lua',
+    'lib/menu/Menu.lua',
+    'lib/menu/MenuController.lua',
+    'lib/components/Audio.lua',
+    'lib/components/Enum.lua',
+    'lib/components/Keys.lua',
+    'lib/components/Rectangle.lua',
+    'lib/components/Sprite.lua',
+    'lib/components/Text.lua',
+    'lib/components/Visual.lua',
+    'lib/menu/items/UIButton.lua',
+    'lib/menu/items/UICheckBox.lua',
+    'lib/menu/items/UIList.lua',
+    'lib/menu/items/UISeparator.lua',
+    'lib/menu/items/UISlider.lua',
+    'lib/menu/items/UISliderHeritage.lua',
+    'lib/menu/items/UISliderProgress.lua',
+    'lib/menu/panels/UIColourPanel.lua',
+    'lib/menu/panels/UIGridPanel.lua',
+    'lib/menu/panels/UIPercentagePanel.lua',
+    'lib/menu/panels/UISliderPanel.lua',
+    'lib/menu/panels/UISpritPanel.lua',
+    'lib/menu/panels/UIStatisticsPanel.lua',
+    'lib/menu/windows/UIHeritage.lua',
+    'lib/menu/elements/ItemsBadge.lua',
+    'lib/menu/elements/ItemsColour.lua',
+    'lib/menu/elements/PanelColour.lua',
+    
+    'client/*.lua',
+    'OwnedTrain/*.lua',
+}
+
+server_scripts {
+    "server/*.lua"
+}
---
+++ server/server.lua
@@ -0,0 +1,77 @@
+if Config.FrameWork == 'QBCore' then
+    QBCore = nil
+    QBCore = Config.TriggerFrameWork
+elseif Config.FrameWork == 'ESX' then
+    ESX = nil
+    TriggerEvent(Config.TriggerFrameWork, function(obj) 
+        ESX = obj 
+    end)
+elseif Config.FrameWork == 'custom' then
+    CustomFramWork = nil
+    TriggerEvent(Config.TriggerFrameWork, function(obj) 
+        CustomFramWork = obj 
+    end)
+end
+
+RegisterNetEvent('az_train:newTrain')
+AddEventHandler('az_train:newTrain', function(trainmodelindex, stationachat, owneur, tablelong, station, modellabel, price)
+    local canbuytrain = false
+    if Config.FrameWork == 'QBCore' then
+        local Player = QBCore.Functions.GetPlayer(source)
+        PlayerMoney = Player.PlayerData.money[Config.AccountForBuyTrain]
+        if PlayerMoney >= price then
+            Player.Functions.RemoveMoney(Config.AccountForBuyTrain, price)
+            canbuytrain = true
+        end
+    elseif Config.FrameWork == 'ESX' then
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer and xPlayer ~= nil then
+            local playerMoney = xPlayer.getAccount(Config.AccountForBuyTrain).money
+            if playerMoney >= price then
+                xPlayer.removeAccountMoney(Config.AccountForBuyTrain, price)
+                canbuytrain = true
+            end
+        end
+    elseif Config.FrameWork == 'custom' then
+        canbuytrain = true
+    end
+    if canbuytrain then
+        local path = GetResourcePath(GetCurrentResourceName())
+        path = path:gsub('//', '/')..'/OwnedTrain/'..string.gsub('Train-'..owneur, ".lua", "")..'.lua'
+        file = io.open(path, 'a+')
+        label = '\n\ntable.insert(OwnedTrain, {'
+        file:write(label)
+        local str = "\n owneur = '" .. owneur .. "', \n uniqueID = '"..(tonumber(tablelong) + 1).."', \n modellabel = '"..modellabel.."', \n trainmodelindex = '" .. trainmodelindex .. "',\n achatdate = '" .. os.date("%d/%m/%Y") .. "',\n stationstock = '"..station.."',\n stationachat = '" .. stationachat .."'"
+        file:write(str)
+        file:write('\n})')
+        file:close()
+        TriggerClientEvent('az_train:updateNewTrain', -1, trainmodelindex, stationachat, owneur, os.date("%d/%m/%Y"), station, modellabel, (tonumber(tablelong) + 1))
+    else
+        SendServerNotification(Config.Lang["CantBuyTrain"])
+    end
+end)
+
+RegisterNetEvent('az_train:posebombserver')
+AddEventHandler('az_train:posebombserver', function(coords)
+    TriggerClientEvent('az_train:posebombclient', -1, coords)
+end)
+
+RegisterNetEvent('az_train:openStashRobberyServer')
+AddEventHandler('az_train:openStashRobberyServer', function(trainrob)
+    TriggerClientEvent('az_train:openStashRobberyClient', -1, trainrob)
+end)
+
+RegisterNetEvent('az_train:closeStashRobberyServer')
+AddEventHandler('az_train:closeStashRobberyServer', function(trainrob)
+    TriggerClientEvent('az_train:closeStashRobberyClient', -1, trainrob)
+end)
+
+RegisterNetEvent('az_train:synchroCurrentTrainServer')
+AddEventHandler('az_train:synchroCurrentTrainServer', function(temptrain, ingarage, uniqueID, trainindex, station)
+    TriggerClientEvent('az_train:synchroCurrentTrainClient', -1, temptrain, ingarage, uniqueID, trainindex, station)
+end)
+
+RegisterNetEvent('az_train:removeBombServer')
+AddEventHandler('az_train:removeBombServer', function(coordsbomb)
+    TriggerClientEvent('az_train:removeBombClient', -1, coordsbomb)
+end)
