ea6f3475bf9752b0d52fa9bc7cc729c08c74ec04
Azeroth <76072277+AzerothSixhten@users.noreply.github.com>
2022-07-28 12:02:21 +0200

    Add files via upload

---
+++ OwnedTrain/Train-Azeroth.lua
@@ -0,0 +1,11 @@
+
+
+table.insert(OwnedTrain, {
+ owneur = 'Azeroth', 
+ uniqueID = '1', 
+ modellabel = 'Oil Train', 
+ trainmodelindex = '19',
+ achatdate = '28/07/2022',
+ stationstock = 'Señora Freeway',
+ stationachat = 'Stock'
+})
